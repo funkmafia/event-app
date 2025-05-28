8a09645ee21dd358946b45caca1e20f8a0269768
Zak <zak.hajjaji@icloud.com>
2025-05-28 15:16:35 +0100

    Nav & Footer components created and impleted to layout.js

---
+++ app/components/Nav.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <nav className="bg-blue-600 text-white py-4 px-8 flex justify-between items-center shadow-md">
+      <Link href="/" className="text-xl font-bold">
+        Project Title
+      </Link>
+      <div className="space-x-6">
+        <Link href="/" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/register" className="hover:underline">
+          Register
+        </Link>
+        <Link href="login" className="hover:underline">
+          Login
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
